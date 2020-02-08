398e9ab175016c777e2ccc0ac0a97ebd7d4a9839
Vrund Patel <vrund1999@yahoo.com>
2020-02-07 20:47:45 -0500

    Parse the title of the webpage, the description, and the table columns

--- scrape.py
+++ scrape.py
@@ -12,3 +12,17 @@ response = requests.get("https://www.slickcharts.com/sp500").text
 
 soup = BeautifulSoup(response, "lxml")
 
+#Title of the webpage
+headline = soup.find('h3', class_='text-center').text
+
+#Description of the webpage
+description_div = soup.find('div', class_='shadow p-3 mb-5 bg-white rounded')
+description = (description_div.p.text)
+
+
+#Table column names
+column_html = soup.find('table', class_='table table-hover table-borderless table-sm')
+column_head = column_html.thead
+column_headers = []
+for column in column_head.find_all('th'):
+	column_headers.append(column.text.strip())
